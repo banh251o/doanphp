fb1a9f5347dcc3161aca234651da49db3b4c0005
votruongthinh <143499278+votruongthinh@users.noreply.github.com>
2025-04-12 23:12:43 +0700

    first commit thinh

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cộng đồng PHP</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header>
         <div class="container header-container">
@@ -22,6 +24,7 @@
                         <li><a href="about.php">Giới thiệu</a></li>
                         <li><a href="posts.php">Bài viết</a></li>
                         <li><a href="add_post.php">Đăng bài</a></li>
+                        <li><a href="login.php">Đăng Nhập</a></li>
                     </ul>
                 </nav>
             </div>
